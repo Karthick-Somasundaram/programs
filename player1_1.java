7f785b0ca5d0dcf5892adf903ab289c4738c6277
Karthick Somasundaram <35144095+Karthick-Somasundaram@users.noreply.github.com>
2018-01-28 21:17:13 +0530

    Create player1_1.java

---
+++ player1_1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class set1_1 {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String s=sc.next();
+	for(int i=0,j=0;i<s.length();i++,j++){
+		System.out.print(s.charAt(s.length()-j-1));
+	}
+	}
+}
