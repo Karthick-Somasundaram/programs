8ec474cc13a5380ed7e14fed71e5074513087b75
Karthick Somasundaram <35144095+Karthick-Somasundaram@users.noreply.github.com>
2018-01-28 21:22:32 +0530

    Create player1_4.java

---
+++ player1_4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class player1_4 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String stl=sc.nextLine();
+		String a=".";
+		System.out.print(stl+a);
+
+	}
+
+}
