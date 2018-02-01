44ea6e24698016d5e858f89bb6c916d8a9d6d4f6
Karthick Somasundaram <35144095+Karthick-Somasundaram@users.noreply.github.com>
2018-02-01 20:42:56 +0530

    Create guvi_set1_2.java

---
+++ guvi_set1_2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class guvi_set1_2{
+public static void main(String[] args){
+Scanner sc=new Scanner (System.in);
+int N=sc.nextInt();
+if(N==0){
+System.out.println("Zero");
+}else if(N>0){
+System.out.println("Positive");
+}else{
+System.out.println("Negative");
+}
+}
+}
